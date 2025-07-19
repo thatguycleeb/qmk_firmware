8dadc0974f81e011f1d1e27569ebe6f144acc340
thatguycleeb <16028559+thatguycleeb@users.noreply.github.com>
2025-07-19 02:01:32 +0200

    added personal boards under 'thatguycleeb'

---
+++ keyboards/thatguycleeb/alan/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#pragma once
+
+/* Split keyboard specifics */
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+
+#define MASTER_RIGHT
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+#endif
